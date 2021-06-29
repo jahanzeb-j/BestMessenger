ae36118d371f38cc92e997bb731df4713b3b6ae8
jahanzebj <jahanzeb.jabbar@conurets.com>
2021-06-29 12:35:05 +0500

    set env

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const db = new Sequelize("messenger", "postgres","admin", {
+  host: "localhost",
+  logging: true,
+  dialect: "postgres",
 });
 
 module.exports = db;
